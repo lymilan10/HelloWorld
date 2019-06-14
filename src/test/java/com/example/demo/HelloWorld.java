dfe8e771a25de2a0f3599e4ae994983a18a3d0c2
liuya <lymilan10@163.com>
2019-06-14 11:05:36 +0800

    age 属性

--- src/test/java/com/example/demo/HelloWorld.java
+++ src/test/java/com/example/demo/HelloWorld.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 public class HelloWorld {
 
+    private int age;
     private String name;
     public static void main(String[] args) {
         System.out.println("Hello World");
