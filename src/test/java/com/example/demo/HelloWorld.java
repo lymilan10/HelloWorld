b03d50af2f4038edc1008ccef6af41790bbae19f
liuya <lymilan10@163.com>
2019-06-14 10:56:51 +0800

    name 属性

--- src/test/java/com/example/demo/HelloWorld.java
+++ src/test/java/com/example/demo/HelloWorld.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
 public class HelloWorld {
+
+    private String name;
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
 }
