0fcb9ac7c0c6cc514f3d6343a434a2cfe17d1e6e
liuya <lymilan10@163.com>
2019-06-14 13:21:59 +0800

    gender 属性

---
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class HelloWorld {
+
+    private int age;
+    private String name;
+    private String gender;
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
